aba5db29500e8ca259b7358da1ce8419a0da3acc
Cory Spicer <corys4139@gmail.com>
2020-10-12 23:01:17 -0400

    p code dream_builder

---
+++ dream_builder.py
@@ -0,0 +1,20 @@
+# dreams are divs that get clicked on to be put in a bin
+# to do that, they will have an on-off switch?
+
+# Dream_board = ['', '', '', '', '', '', '', '', '', '', '', '']
+# then map the dream board to divs in html that have the symbols on them
+# when clicking on the symbol, that index of the dream_board gets updated
+# and on the page the symbol changes color
+# how to change what goes in the dream_board[idx] so each symbol has a unique entry?
+# dream_board[idx] = '{idx}' ??
+
+# what about a handle function on the onClick that does all of this? changes the color, updates the db idx,
+# maybe save too?
+
+# when page is loaded, the dream_board is reset 
+# we probably need a dream_board model: 
+# dream_board with 12 boolean rows
+# dream_board gets saved when submitted
+# one user has many dbs
+
+# dream_point = input('choose 1 - 12: ')
